94ce0de02fe4b5478aee597e4ba3b903515d7eec
usamaghawte <usamaghawte16@gmail.com>
2017-01-18 13:16:10 +0530

    Switch added

--- src/main/java/com/morningstar/searchTeam/App.java
+++ src/main/java/com/morningstar/searchTeam/App.java
@@ -8,6 +8,33 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        		while (true) {
+			System.out.println("********BASIC PROGRAMING*****");
+			System.out.println("\n 1:PRIME NUMBER \n 2:FIBONACCI SERIES \n 3:PALINDROME \n 4:ARMSTRONG NUMBER \n 5:Exit");
+			System.out.println("  \nSelect your choice:");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				System.out.println("@@@@@PRIME NUMBER@@@@@");
+				break;
+
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				System.out.println("Terminated");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("WRONG CHOICE");
+				break;
+
+			}
+
+		}
+
     }
 }
