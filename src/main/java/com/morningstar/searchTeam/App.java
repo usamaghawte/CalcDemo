a1ce36871191616a5e8b71e9d962efc50488daec
mandar-deshpande <mandar.deshpande@morningstar.com>
2017-01-18 14:54:23 +0530

    Palindrome and Fibonacci Series is added Without Test Case

--- src/main/java/com/morningstar/searchTeam/App.java
+++ src/main/java/com/morningstar/searchTeam/App.java
@@ -1,40 +1,61 @@
 package com.morningstar.searchTeam;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        		while (true) {
-			System.out.println("********BASIC PROGRAMING*****");
-			System.out.println("\n 1:PRIME NUMBER \n 2:FIBONACCI SERIES \n 3:PALINDROME \n 4:ARMSTRONG NUMBER \n 5:Exit");
-			System.out.println("  \nSelect your choice:");
-			int choice = sc.nextInt();
-			switch (choice) {
-			case 1:
-				System.out.println("@@@@@PRIME NUMBER@@@@@");
-				break;
+public class App {
+	private static Scanner sc;
 
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				System.out.println("Terminated");
-				System.exit(0);
-				break;
-			default:
-				System.out.println("WRONG CHOICE");
-				break;
+	public static void main(String[] args) {
 
+		sc = new Scanner(System.in);
+		MaddyWork md = new MaddyWork();
+		while (true) {
+			try {
+				System.out.println("********BASIC PROGRAMING*****");
+				System.out.println(
+						"\n 1:PRIME NUMBER \n 2:FIBONACCI SERIES \n 3:PALINDROME \n 4:ARMSTRONG NUMBER \n 5:Exit");
+				System.out.println("  \nSelect your choice:");
+				int choice = sc.nextInt();
+				switch (choice) {
+				case 1:
+					System.out.println("@@@@@PRIME NUMBER@@@@@");
+					break;
+				case 2:
+					System.out.println("@@@@@ PALINDROME @@@@@");
+					System.out.println("Enter Data to Check Palindrome");
+					String palin = sc.nextLine().trim();
+					if (md.palindrome(palin))
+						System.out.println(palin + " is Palindrome");
+					else
+						System.out.println(palin + " is not a Palindrome");
+					break;
+				case 3:
+					System.out.println("@@@@@ FIBONACCI SERIES @@@@@");
+					System.out.println("Enter First Number of Fibonnaci Series");
+					int f1 = sc.nextInt();
+					System.out.println("Enter Second Number of Fibonnaci Series");
+					int f2 = sc.nextInt();
+					md.fibonnaciS(f1, f2);
+					break;
+				case 4:
+					break;
+				case 5:
+					System.out.println("Terminated");
+					System.exit(0);
+					break;
+				default:
+					System.out.println("WRONG CHOICE");
+					break;
+
+				}
+			} catch (Exception ex) {
+				System.err.println(ex);
 			}
-
 		}
 
-    }
+	}
 }
---
+++ src/main/java/com/morningstar/searchTeam/api/MaddyWorkApi.java
@@ -0,0 +1,7 @@
+package com.morningstar.searchTeam.api;
+
+public interface MaddyWorkApi {
+	public boolean palindrome(String data);
+
+	public void fibonnaciS(int a, int b);
+}
